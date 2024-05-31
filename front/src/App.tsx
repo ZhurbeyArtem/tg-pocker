259fa48e9f402e9965cb58eb94a32d451c3a52b0
Artem Zhurbei <zhurbeyartem@gmail.com>
2024-06-01 00:00:26 +0300

    test

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from './routers'
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
